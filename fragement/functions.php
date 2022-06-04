e9eb1952d84fc9855c9768d9b1c1a07bf15fd6b3
dollarstir <kpin463@gmail.com>
2022-06-04 21:45:25 +0000

    dsfds

---
+++ fragement/functions.php
@@ -0,0 +1,161 @@
+<?php
+
+// created on 4th June , 2022
+// Author : Frederick Ennin (Dollarstir)
+//
+// Email: kpin463@gmail.com
+
+function begin($title)
+{
+    echo '<!doctype html>
+    <html class="no-js" lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Nasea Book - '.$title.'</title>
+        <meta name="description" content="Nasea books is one of the Best E-book stores in the world">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="main/img/favicon.png">
+    
+        <!-- all css here -->
+        <!-- bootstrap v3.3.6 css -->
+        <link rel="stylesheet" href="main/css/bootstrap.min.css">
+        <!-- animate css -->
+        <link rel="stylesheet" href="main/css/animate.css">
+        <!-- meanmenu css -->
+        <link rel="stylesheet" href="main/css/meanmenu.min.css">
+        <!-- owl.carousel css -->
+        <link rel="stylesheet" href="main/css/owl.carousel.css">
+        <!-- font-awesome css -->
+        <link rel="stylesheet" href="main/css/font-awesome.min.css">
+        <!-- flexslider.css-->
+        <link rel="stylesheet" href="main/css/flexslider.css">
+        <!-- chosen.min.css-->
+        <link rel="stylesheet" href="main/css/chosen.min.css">
+        <!-- style css -->
+        <link rel="stylesheet" href="main/style.css">
+        <!-- responsive css -->
+        <link rel="stylesheet" href="main/css/responsive.css">
+        <!-- modernizr css -->
+        <script src="main/js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>';
+}
+
+function footer()
+{
+    echo '  <footer>
+    <!-- footer-top-start -->
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-top-menu bb-2">
+                        <nav>
+                            <ul>
+                                <li><a href="main/#">home</a></li>
+                                <li><a href="main/#">Enable Cookies</a></li>
+                                <li><a href="main/#">Privacy and Cookie Policy</a></li>
+                                <li><a href="main/#">contact us</a></li>
+                                <li><a href="main/#">blog</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer-top-start -->
+    <!-- footer-mid-start -->
+    <div class="footer-mid ptb-50">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-12">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-12">
+                            <div class="single-footer br-2 xs-mb">
+                                <div class="footer-title mb-20">
+                                    <h3>Products</h3>
+                                </div>
+                                <div class="footer-mid-menu">
+                                    <ul>
+                                        <li><a href="main/about.html">About us</a></li>
+                                        <li><a href="main/#">Prices drop </a></li>
+                                        <li><a href="main/#">New products</a></li>
+                                        <li><a href="main/#">Best sales</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-12">
+                            <div class="single-footer br-2 xs-mb">
+                                <div class="footer-title mb-20">
+                                    <h3>Our company</h3>
+                                </div>
+                                <div class="footer-mid-menu">
+                                    <ul>
+                                        <li><a href="main/contact.html">Contact us</a></li>
+                                        <li><a href="main/#">Sitemap</a></li>
+                                        <li><a href="main/#">Stores</a></li>
+                                        <li><a href="main/register.html">My account </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-12">
+                            <div class="single-footer br-2 xs-mb">
+                                <div class="footer-title mb-20">
+                                    <h3>Your account</h3>
+                                </div>
+                                <div class="footer-mid-menu">
+                                    <ul>
+                                        <li><a href="main/contact.html">Addresses</a></li>
+                                        <li><a href="main/#">Credit slips </a></li>
+                                        <li><a href="main/#"> Orders</a></li>
+                                        <li><a href="main/#">Personal info</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="single-footer mrg-sm">
+                        <div class="footer-title mb-20">
+                            <h3>STORE INFORMATION</h3>
+                        </div>
+                        <div class="footer-contact">
+                            <p class="adress">
+                                <span>My Company</span>
+                                Your address goes here.
+                            </p>
+                            <p><span>Call us now:</span> 0123456789</p>
+                            <p><span>Email:</span> demo@example.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer-mid-end -->
+    <!-- footer-bottom-start -->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row bt-2">
+                <div class="col-lg-6 col-md-6 col-12">
+                    <div class="copy-right-area">
+                        <p>&copy; 2021 <strong> Koparion </strong> Mede with ❤️ by <a href="https://github.com/dollarstir" target="_blank"><strong>Dollarsoft</strong></a></p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <div class="payment-img text-right">
+                        <a href="main/#"><img src="main/img/1.png" alt="payment" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer-bottom-end -->
+</footer>';
+}
