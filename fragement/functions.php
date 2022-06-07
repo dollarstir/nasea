30a617550e02c556717f8765d2eaefb955ed042a
dollarstir <kpin463@gmail.com>
2022-06-07 19:07:20 +0000

    mainmenu

--- fragement/functions.php
+++ fragement/functions.php
@@ -268,6 +268,165 @@ function topbars()
     <!-- header-mid-area-end -->';
 }
 
+function mainmenu()
+{
+    return '<!-- main-menu-area-start -->
+    <div class="main-menu-area d-md-none d-none d-lg-block sticky-header-1" id="header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="menu-area">
+                        <nav>
+                            <ul>
+                                <li class="active"><a href="main/index.html">Home<i class="fa fa-angle-down"></i></a>
+                                    <div class="sub-menu">
+                                        <ul>
+                                            <li><a href="main/index.html">Home-1</a></li>
+                                            <li><a href="main/index-2.html">Home-2</a></li>
+                                            <li><a href="main/index-3.html">Home-3</a></li>
+                                            <li><a href="main/index-4.html">Home-4</a></li>
+                                            <li><a href="main/index-5.html">Home-5</a></li>
+                                            <li><a href="main/index-6.html">Home-6</a></li>
+                                            <li><a href="main/index-7.html">Home-7</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li><a href="main/product-details.html">Book<i class="fa fa-angle-down"></i></a>
+                                    <div class="mega-menu">
+                                        <span>
+                                            <a href="main/#" class="title">Jackets</a>
+                                            <a href="main/shop.html">Tops & Tees</a>
+                                            <a href="main/shop.html">Polo Short Sleeve</a>
+                                            <a href="main/shop.html">Graphic T-Shirts</a>
+                                            <a href="main/shop.html">Jackets & Coats</a>
+                                            <a href="main/shop.html">Fashion Jackets</a>
+                                        </span>
+                                        <span>
+                                            <a href="main/#" class="title">weaters</a>
+                                            <a href="main/shop.html">Crochet</a>
+                                            <a href="main/shop.html">Sleeveless</a>
+                                            <a href="main/shop.html">Stripes</a>
+                                            <a href="main/shop.html">Sweaters</a>
+                                            <a href="main/shop.html">hoodies</a>
+                                        </span>
+                                        <span>
+                                            <a href="main/#" class="title">Bottoms</a>
+                                            <a href="main/shop.html">Heeled sandals</a>
+                                            <a href="main/shop.html">Polo Short Sleeve</a>
+                                            <a href="main/shop.html">Flat sandals</a>
+                                            <a href="main/shop.html">Short Sleeve</a>
+                                            <a href="main/shop.html">Long Sleeve</a>
+                                        </span>
+                                        <span>
+                                            <a href="main/#" class="title">Jeans Pants</a>
+                                            <a href="main/shop.html">Polo Short Sleeve</a>
+                                            <a href="main/shop.html">Sleeveless</a>
+                                            <a href="main/shop.html">Graphic T-Shirts</a>
+                                            <a href="main/shop.html">Hoodies</a>
+                                            <a href="main/shop.html">Jackets</a>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li><a href="main/product-details.html">Audio books<i class="fa fa-angle-down"></i></a>
+                                    <div class="mega-menu">
+                                        <span>
+                                            <a href="main/#" class="title">Shirts</a>
+                                            <a href="main/shop.html">Tops & Tees</a>
+                                            <a href="main/shop.html">Sweaters </a>
+                                            <a href="main/shop.html">Hoodies</a>
+                                            <a href="main/shop.html">Jackets & Coats</a>
+                                        </span>
+                                        <span>
+                                            <a href="main/#" class="title">Tops & Tees</a>
+                                            <a href="main/shop.html">Long Sleeve </a>
+                                            <a href="main/shop.html">Short Sleeve</a>
+                                            <a href="main/shop.html">Polo Short Sleeve</a>
+                                            <a href="main/shop.html">Sleeveless</a>
+                                        </span>
+                                        <span>
+                                            <a href="main/#" class="title">Jackets</a>
+                                            <a href="main/shop.html">Sweaters</a>
+                                            <a href="main/shop.html">Hoodies</a>
+                                            <a href="main/shop.html">Wedges</a>
+                                            <a href="main/shop.html">Vests</a>
+                                        </span>
+                                        <span>
+                                            <a href="main/#" class="title">Jeans Pants</a>
+                                            <a href="main/shop.html">Polo Short Sleeve</a>
+                                            <a href="main/shop.html">Sleeveless</a>
+                                            <a href="main/shop.html">Graphic T-Shirts</a>
+                                            <a href="main/shop.html">Hoodies</a>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li><a href="main/product-details.html">children’s books<i class="fa fa-angle-down"></i></a>
+                                    <div class="mega-menu mega-menu-2">
+                                        <span>
+                                            <a href="main/#" class="title">Tops</a>
+                                            <a href="main/shop.html">Shirts</a>
+                                            <a href="main/shop.html">Florals</a>
+                                            <a href="main/shop.html">Crochet</a>
+                                            <a href="main/shop.html">Stripes</a>
+                                        </span>
+                                        <span>
+                                            <a href="main/#" class="title">Bottoms</a>
+                                            <a href="main/shop.html">Shorts</a>
+                                            <a href="main/shop.html">Dresses</a>
+                                            <a href="main/shop.html">Trousers</a>
+                                            <a href="main/shop.html">Jeans</a>
+                                        </span>
+                                        <span>
+                                            <a href="main/#" class="title">Shoes</a>
+                                            <a href="main/shop.html">Heeled sandals</a>
+                                            <a href="main/shop.html">Flat sandals</a>
+                                            <a href="main/shop.html">Wedges</a>
+                                            <a href="main/shop.html">Ankle boots</a>
+                                        </span>
+                                    </div>
+                                </li>
+                                <li><a href="main/#">blog<i class="fa fa-angle-down"></i></a>
+                                    <div class="sub-menu sub-menu-2">
+                                        <ul>
+                                            <li><a href="main/blog.html">blog</a></li>
+                                            <li><a href="main/blog-details.html">blog-details</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li><a href="main/#">pages<i class="fa fa-angle-down"></i></a>
+                                    <div class="sub-menu sub-menu-2">
+                                        <ul>
+                                            <li><a href="main/shop.html">shop</a></li>
+                                            <li><a href="main/shop-list.html">shop list view</a></li>
+                                            <li><a href="main/product-details.html">product-details</a></li>
+                                            <li><a href="main/product-details-affiliate.html">product-affiliate</a></li>
+                                            <li><a href="main/blog.html">blog</a></li>
+                                            <li><a href="main/blog-details.html">blog-details</a></li>
+                                            <li><a href="main/contact.html">contact</a></li>
+                                            <li><a href="main/about.html">about</a></li>
+                                            <li><a href="main/login.html">login</a></li>
+                                            <li><a href="main/register.html">register</a></li>
+                                            <li><a href="main/my-account.html">my-account</a></li>
+                                            <li><a href="main/cart.html">cart</a></li>
+                                            <li><a href="main/compare.html">compare</a></li>
+                                            <li><a href="main/checkout.html">checkout</a></li>
+                                            <li><a href="main/wishlist.html">wishlist</a></li>
+                                            <li><a href="main/404.html">404 Page</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="safe-area">
+                        <a href="main/product-details.html">sales off</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- main-menu-area-end -->';
+}
+
 // logical navbar by dollarstir
 function navbar($pid)
 {
