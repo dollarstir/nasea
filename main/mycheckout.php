3d74361a9a8b8cff81e9aae7d6fe4de9f47c7abb
dollarstir <kpin463@gmail.com>
2022-06-08 10:16:10 +0000

    adss

--- main/mycheckout.php
+++ main/mycheckout.php
@@ -413,33 +413,6 @@ begin('Checkout');
 		
 		
 		<!-- all js here -->
-		<!-- jquery latest version -->
-        <script src="js/vendor/jquery-1.12.4.min.js"></script>
-		<!-- popper js -->
-        <script src="js/popper.min.js"></script>
-		<!-- bootstrap js -->
-        <script src="js/bootstrap.min.js"></script>
-		<!-- owl.carousel js -->
-        <script src="js/owl.carousel.min.js"></script>
-		<!-- meanmenu js -->
-        <script src="js/jquery.meanmenu.js"></script>
-		<!-- wow js -->
-        <script src="js/wow.min.js"></script>
-		<!-- jquery.parallax-1.1.3.js -->
-        <script src="js/jquery.parallax-1.1.3.js"></script>
-		<!-- jquery.countdown.min.js -->
-        <script src="js/jquery.countdown.min.js"></script>
-		<!-- jquery.flexslider.js -->
-        <script src="js/jquery.flexslider.js"></script>
-		<!-- chosen.jquery.min.js -->
-        <script src="js/chosen.jquery.min.js"></script>
-		<!-- jquery.counterup.min.js -->
-        <script src="js/jquery.counterup.min.js"></script>
-		<!-- waypoints.min.js -->
-        <script src="js/waypoints.min.js"></script>
-		<!-- plugins js -->
-        <script src="js/plugins.js"></script>
-		<!-- main js -->
-        <script src="js/main.js"></script>
+		<?php scripts(); ?>
     </body>
 </html>
