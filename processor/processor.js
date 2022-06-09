73c0bab5ffd62e192f3d54213d106579af8382f9
dollarstir <kpin463@gmail.com>
2022-06-09 12:21:53 +0000

    sdfds

--- processor/processor.js
+++ processor/processor.js
@@ -240,6 +240,31 @@ $('.removefromcart').click(function(e){
 
 
 
+// apply coupon
+
+
+$('.addtocart').click(function(e){
+
+  e.preventDefault();
+  // before();
+  var id = $(this).attr('id');
+  var staff = {
+      url: 'processor/processor.php?action=addtocart&id=1',
+      type: 'post',
+      data: {"id": id},
+      // cache: false,
+      // contentType: false,
+      // processData: false,
+      beforeSend: before,
+      success: resp
+
+  };
+  $.ajax(staff);
+});
+
+
+
+
 
 
 
