61bd8bd731fc66f4f8b866928e7e6782a9da5b21
dollarstir <kpin463@gmail.com>
2022-06-17 13:12:15 +0000

    sads

--- main/auth.php
+++ main/auth.php
@@ -23,21 +23,7 @@ $a = aboutpage();
 	</header>
 	<!-- header-area-end -->
 	<!-- breadcrumbs-area-start -->
-	<div class="breadcrumbs-area mb-70">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="breadcrumbs-menu">
-						<ul>
-							<li><a href="home">Home</a></li>
-							<li><a href="login" class="active">login</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- breadcrumbs-area-end -->
+	
 	<!-- user-login-area-start -->
 	<div class="user-login-area mb-70">
 		<div class="container">
@@ -45,19 +31,19 @@ $a = aboutpage();
 				<div class="col-lg-12">
 					<div class="login-title text-center mb-30">
 						<h2>Login</h2>
-						<p>doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo<br>inventore veritatis et
-							quasi architecto beat</p>
+						<!-- <p>doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo<br>inventore veritatis et
+							quasi architecto beat</p> -->
 					</div>
 				</div>
 				<div class="offset-lg-3 col-lg-6 col-md-12 col-12">
 					<div class="login-form">
 						<div class="single-login">
-							<label>Username or email<span>*</span></label>
-							<input type="text" />
+							<label>Phone Number or email<span>*</span></label>
+							<input type="text" name="login" />
 						</div>
 						<div class="single-login">
 							<label>Passwords <span>*</span></label>
-							<input type="text" />
+							<input type="password" />
 						</div>
 						<div class="single-login single-login-2">
 							<a href="#">login</a>
@@ -72,152 +58,12 @@ $a = aboutpage();
 	</div>
 	<!-- user-login-area-end -->
 	<!-- footer-area-start -->
-	<footer>
-		<!-- footer-top-start -->
-		<div class="footer-top">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="footer-top-menu bb-2">
-							<nav>
-								<ul>
-									<li><a href="#">home</a></li>
-									<li><a href="#">Enable Cookies</a></li>
-									<li><a href="#">Privacy and Cookie Policy</a></li>
-									<li><a href="#">contact us</a></li>
-									<li><a href="#">blog</a></li>
-								</ul>
-							</nav>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- footer-top-start -->
-		<!-- footer-mid-start -->
-		<div class="footer-mid ptb-50">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-8 col-md-12">
-						<div class="row">
-							<div class="col-lg-4 col-md-4 col-12">
-								<div class="single-footer br-2 xs-mb">
-									<div class="footer-title mb-20">
-										<h3>Products</h3>
-									</div>
-									<div class="footer-mid-menu">
-										<ul>
-											<li><a href="about.html">About us</a></li>
-											<li><a href="#">Prices drop </a></li>
-											<li><a href="#">New products</a></li>
-											<li><a href="#">Best sales</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-4 col-md-4 col-12">
-								<div class="single-footer br-2 xs-mb">
-									<div class="footer-title mb-20">
-										<h3>Our company</h3>
-									</div>
-									<div class="footer-mid-menu">
-										<ul>
-											<li><a href="contact.html">Contact us</a></li>
-											<li><a href="#">Sitemap</a></li>
-											<li><a href="#">Stores</a></li>
-											<li><a href="register.html">My account </a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-4 col-md-4 col-12">
-								<div class="single-footer br-2 xs-mb">
-									<div class="footer-title mb-20">
-										<h3>Your account</h3>
-									</div>
-									<div class="footer-mid-menu">
-										<ul>
-											<li><a href="contact.html">Addresses</a></li>
-											<li><a href="#">Credit slips </a></li>
-											<li><a href="#"> Orders</a></li>
-											<li><a href="#">Personal info</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-12">
-						<div class="single-footer mrg-sm">
-							<div class="footer-title mb-20">
-								<h3>STORE INFORMATION</h3>
-							</div>
-							<div class="footer-contact">
-								<p class="adress">
-									<span>My Company</span>
-									Your address goes here.
-								</p>
-								<p><span>Call us now:</span> 0123456789</p>
-								<p><span>Email:</span> demo@example.com</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- footer-mid-end -->
-		<!-- footer-bottom-start -->
-		<div class="footer-bottom">
-			<div class="container">
-				<div class="row bt-2">
-					<div class="col-lg-6 col-md-6 col-12">
-						<div class="copy-right-area">
-							<p>&copy; 2021 <strong> Koparion </strong> Mede with ❤️ by <a href="https://hasthemes.com/"
-									target="_blank"><strong>HasThemes</strong></a></p>
-						</div>
-					</div>
-					<div class="col-lg-6 col-md-6 col-12">
-						<div class="payment-img text-right">
-							<a href="#"><img src="img/1.png" alt="payment" /></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- footer-bottom-end -->
-	</footer>
+	<?php footer(); ?>
 	<!-- footer-area-end -->
 
 
 	<!-- all js here -->
-	<!-- jquery latest version -->
-	<script src="js/vendor/jquery-1.12.4.min.js"></script>
-	<!-- popper js -->
-	<script src="js/popper.min.js"></script>
-	<!-- bootstrap js -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- owl.carousel js -->
-	<script src="js/owl.carousel.min.js"></script>
-	<!-- meanmenu js -->
-	<script src="js/jquery.meanmenu.js"></script>
-	<!-- wow js -->
-	<script src="js/wow.min.js"></script>
-	<!-- jquery.parallax-1.1.3.js -->
-	<script src="js/jquery.parallax-1.1.3.js"></script>
-	<!-- jquery.countdown.min.js -->
-	<script src="js/jquery.countdown.min.js"></script>
-	<!-- jquery.flexslider.js -->
-	<script src="js/jquery.flexslider.js"></script>
-	<!-- chosen.jquery.min.js -->
-	<script src="js/chosen.jquery.min.js"></script>
-	<!-- jquery.counterup.min.js -->
-	<script src="js/jquery.counterup.min.js"></script>
-	<!-- waypoints.min.js -->
-	<script src="js/waypoints.min.js"></script>
-	<!-- plugins js -->
-	<script src="js/plugins.js"></script>
-	<!-- main js -->
-	<script src="js/main.js"></script>
+	<?php scripts(); ?>
 </body>
 
 </html>
