5786b13aeda3606b73a413f5f39dcc163d5a36d0
dollarstir <kpin463@gmail.com>
2022-06-23 06:25:48 +0000

    zdsfsd

--- main/paypros.php
+++ main/paypros.php
@@ -4,4 +4,7 @@ session_start();
 $ref = $_SESSION['ref'];
 $amount = $_SESSION['amount'];
 $token = $_SESSION['token'];
-echo $amount;
+$uid = $_SESSION['user']['id'];
+$dateadded = date('jS F, Y');
+$timepaid = date('h : i : s a');
+$a = insert('transactions');
