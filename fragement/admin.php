7ce637a8af28e2d415b1eb86b0a43fb0b7758009
dollarstir <kpin463@gmail.com>
2022-07-06 02:43:27 +0000

    sdf

--- fragement/admin.php
+++ fragement/admin.php
@@ -12,37 +12,37 @@ function adminhead($title)
         <title>Nasea Admin - '.$title.'</title>
     
     
-        <link rel="stylesheet" href="css/bootstrap1.min.css" />
+        <link rel="stylesheet" href="backend/css/bootstrap1.min.css" />
     
-        <link rel="stylesheet" href="vendors/themefy_icon/themify-icons.css" />
+        <link rel="stylesheet" href="backend/vendors/themefy_icon/themify-icons.css" />
     
-        <link rel="stylesheet" href="vendors/niceselect/css/nice-select.css" />
+        <link rel="stylesheet" href="backend/vendors/niceselect/css/nice-select.css" />
     
-        <link rel="stylesheet" href="vendors/owl_carousel/css/owl.carousel.css" />
+        <link rel="stylesheet" href="backend/vendors/owl_carousel/css/owl.carousel.css" />
     
-        <link rel="stylesheet" href="vendors/gijgo/gijgo.min.css" />
+        <link rel="stylesheet" href="backend/vendors/gijgo/gijgo.min.css" />
     
-        <link rel="stylesheet" href="vendors/font_awesome/css/all.min.css" />
-        <link rel="stylesheet" href="vendors/tagsinput/tagsinput.css" />
+        <link rel="stylesheet" href="backend/vendors/font_awesome/css/all.min.css" />
+        <link rel="stylesheet" href="backend/vendors/tagsinput/tagsinput.css" />
     
-        <link rel="stylesheet" href="vendors/datepicker/date-picker.css" />
-        <link rel="stylesheet" href="vendors/vectormap-home/vectormap-2.0.2.css" />
+        <link rel="stylesheet" href="backend/vendors/datepicker/date-picker.css" />
+        <link rel="stylesheet" href="backend/vendors/vectormap-home/vectormap-2.0.2.css" />
     
-        <link rel="stylesheet" href="vendors/scroll/scrollable.css" />
+        <link rel="stylesheet" href="backend/vendors/scroll/scrollable.css" />
     
-        <link rel="stylesheet" href="vendors/datatable/css/jquery.dataTables.min.css" />
-        <link rel="stylesheet" href="vendors/datatable/css/responsive.dataTables.min.css" />
-        <link rel="stylesheet" href="vendors/datatable/css/buttons.dataTables.min.css" />
+        <link rel="stylesheet" href="backend/vendors/datatable/css/jquery.dataTables.min.css" />
+        <link rel="stylesheet" href="backend/vendors/datatable/css/responsive.dataTables.min.css" />
+        <link rel="stylesheet" href="backend/vendors/datatable/css/buttons.dataTables.min.css" />
     
-        <link rel="stylesheet" href="vendors/text_editor/summernote-bs4.css" />
+        <link rel="stylesheet" href="backend/vendors/text_editor/summernote-bs4.css" />
     
-        <link rel="stylesheet" href="vendors/morris/morris.css">
+        <link rel="stylesheet" href="backend/vendors/morris/morris.css">
     
-        <link rel="stylesheet" href="vendors/material_icon/material-icons.css" />
+        <link rel="stylesheet" href="backend/vendors/material_icon/material-icons.css" />
     
-        <link rel="stylesheet" href="css/metisMenu.css">
+        <link rel="stylesheet" href="backend/css/metisMenu.css">
     
-        <link rel="stylesheet" href="css/style1.css" />
-        <link rel="stylesheet" href="css/colors/default.css" id="colorSkinCSS">
+        <link rel="stylesheet" href="backend/css/style1.css" />
+        <link rel="stylesheet" href="backend/css/colors/default.css" id="colorSkinCSS">
     </head>';
 }
