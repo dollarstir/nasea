1a583ac9bb46fcc03acd808c89575c806316c1a5
Joshua Tetteh <joshua.nanakojo.tetteh@gmail.com>
2022-07-17 21:27:11 +0000

    Degugging addtocart function #58

--- fragement/deep.php
+++ fragement/deep.php
@@ -12,10 +12,8 @@
  
 function addtocart($id)
     {
-        require '../core/minified.php';
-
     session_start();
-    $dt = customfetch('books', [['id', '=', $id]]);
+    $dt = customfetch('books', ['id', '=', $id]);
     $book = $dt[0];
 
     if (isset($_SESSION['cart'])) {
