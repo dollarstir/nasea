5beff40fcf4abf049565b89ed271b1c791f2e745
Joshua Tetteh <joshua.nanakojo.tetteh@gmail.com>
2022-07-17 18:13:56 +0000

    Degugging addtocart function #20

--- main/myproduct.php
+++ main/myproduct.php
@@ -58,7 +58,7 @@ echo '<!doctype html>
     </head>';
 
  ?>
-    <body class="product-details">
+    <body class="product-details test-class">
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
