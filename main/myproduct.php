90375967c99c1e71ae89d1a791886c3921d74c80
dollarstir <kpin463@gmail.com>
2022-07-19 09:15:52 +0000

    dsfd

--- main/myproduct.php
+++ main/myproduct.php
@@ -465,7 +465,7 @@ echo '<!doctype html>
 		</div>
 		<!-- product-main-area-end -->
 		<!-- footer-area-start -->
-		<?php footer2(); ?>
+		<?php footer1(); ?>
 		<!-- footer-area-end -->
 		<!-- Modal -->
         <div class="modal fade" id="productModal" tabindex="-1" role="dialog">
