39bc05d422106fda6c56e2a6c1f4022601f5a1f5
dollarstir <kpin463@gmail.com>
2022-07-20 09:54:07 +0000

    fg

--- main/paypros.php
+++ main/paypros.php
@@ -30,10 +30,14 @@ if ($amount == '' || $token == '' || $ref == '') {
         $umess2 = '<html><body><center><img src="https://naseabooks.com/yolkassets/upload/logo.png" style="width:150px;height:100px;"></center><p>Dear '.$ko[0]['name'].', Your order with order number <strong>: '.$ar['orderno'].'</strong> has been completed. </p><p><table style="width:100%;  border-collapse: collapse;border:1px solid;"><tr><th style="border-collapse: collapse;">Book Name</th><th style="border-collapse: collapse;">Price(GHS)</th><th style="border-collapse: collapse;">Discount Price(GHS)</th><th style="border-collapse: collapse;">Discount(%)</th></tr><tr><td style="border-collapse: collapse; border:1px solid;">'.$ar['bookname'].'</td><td style="border-collapse: collapse; border:1px solid;">'.$ar['price'].'</td><td style="border-collapse: collapse; border:1px solid;">'.$ar['discountprice'].'</td><td style="border-collapse: collapse; border:1px solid;">'.$ar['discount'].'</td></tr></table></p></body></html>';
         sms('Naseabooks', $s[0]['authnumber'], $mess);
         sms('Naseabooks', $ko[0]['phone'], $umess);
+        // sending sms and mail to admin
+        sms('Naseabooks', '0240370479', 'New order with no '.$ar['orderno'].' completed');
+        sendmail('naseabooks.com', 'New order', 'New order with no '.$ar['orderno'].' completed', $from_name = 'Nasea Books', ['kpin463@gmail.com,solomon@naseabooks.com'], $reply_to = 'support@naseabooks.com', $reply_to_name = 'Nasea Books');
 
         sendmail('naseabooks.com', 'Book Purchase', $mess2, $from_name = 'Nasea Books', [$s[0]['authemail']], $reply_to = 'support@naseabooks.com', $reply_to_name = 'Nasea Books');
         sendmail('naseabooks.com', 'Order status', $umess2, $from_name = 'Nasea Books', [$ko[0]['email']], $reply_to = 'support@naseabooks.com', $reply_to_name = 'Nasea Books');
     }
+
     unset($_SESSION['ref']);
     unset($_SESSION['amount']);
     unset($_SESSION['token']);
