dd90b5629e75e1067347f9c1c79d9207af1e8ce4
dollarstir <kpin463@gmail.com>
2022-07-21 11:10:13 +0000

    wefew

--- fragement/functions.php
+++ fragement/functions.php
@@ -569,12 +569,12 @@ function topbars()
         <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-5 col-12">
-                    <div class="header-search">
+                    <--<div class="header-search">
                         <form action="#">
                             <input type="text" placeholder="Search entire store here..." />
                             <a href="main/#"><i class="fa fa-search"></i></a>
                         </form>
-                    </div>
+                    </div>-->
                 </div>
                 <div class="col-lg-6 col-md-4 col-12">
                     <div class="logo-area text-center logo-xs-mrg">
