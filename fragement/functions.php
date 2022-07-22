c4d474a8206c71a061578aff316b939ae8209cad
dollarstir <kpin463@gmail.com>
2022-07-22 02:47:53 +0000

    sddf

--- fragement/functions.php
+++ fragement/functions.php
@@ -1008,6 +1008,44 @@ function mycustomepagecount($id, $type, $perpage, $ct)
 
     return $result;
 }
+
+function mycustomepagecount1($id, $type, $perpage, $ct)
+{
+    $res = custompagecount('books', [['author', '=', $id]], '', $perpage, $ct);
+    $total_pages = $res;
+
+    $canto = $ct - 1;
+    if ($type == 'main') {
+        $mimi = ($ct == 1) ? '' : '<li><a href="author/'.$id.'?page='.$canto.'" class="angle"><i class="fa fa-angle-left"></i></a></li>';
+        $result = $mimi;
+        for ($page = 1; $page <= $total_pages; ++$page) {
+            $conto = $ct + 1;
+            $combo = ($ct == $page) ? 'active' : '';
+            $combo2 = ($ct == $total_pages) ? '' : '<li><a href="author/'.$id.'?page='.$conto.'" class="angle"><i class="fa fa-angle-right"></i></a></li>';
+            $result .= '
+                <li><a href="author/'.$id.'?page='.$page.'" class="'.$combo.'">'.$page.'</a></li>
+                  
+                  
+              ';
+        }
+    } elseif ($type == 'sub') {
+        $mimi = ($ct == 1) ? '' : '<li><a href="../author/'.$id.'?page='.$canto.'" class="angle"><i class="fa fa-angle-left"></i></a></li>';
+        $result = $mimi;
+        for ($page = 1; $page <= $total_pages; ++$page) {
+            $conto = $ct + 1;
+            $combo = ($ct == $page) ? 'active' : '';
+            $combo2 = ($ct == $total_pages) ? '' : '<li><a href="../author/'.$id.'?page='.$conto.'" class="angle"><i class="fa fa-angle-right"></i></a></li>';
+            $result .= '
+                <li><a href="../author/'.$id.'?page='.$page.'" class="'.$combo.'">'.$page.'</a></li>
+                  
+                  
+              ';
+        }
+    }
+    $result .= $combo2;
+
+    return $result;
+}
 function app()
 {
     $s = fetchAll('settings');
