0e411e561d86ca2e601028d0d2fbe84cfa4c543f
dollarstir <kpin463@gmail.com>
2022-07-29 13:27:05 +0000

    dsfds

--- fragement/functions.php
+++ fragement/functions.php
@@ -506,7 +506,23 @@ function footer2()
 
 function scripts2()
 {
-    echo '<!-- jquery latest version -->
+    echo '
+    
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src=\'https://embed.tawk.to/62e3d52b54f06e12d88bed32/1g94vhcuj\';
+s1.charset=\'UTF-8\';
+s1.setAttribute(\'crossorigin\',\'*\');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+    
+    <!-- jquery latest version -->
     <script src="../main/js/vendor/jquery-1.12.4.min.js"></script>
     '.Yolk::uicore('jsa').'
     <!-- popper js -->
