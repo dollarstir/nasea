c503c21bb686b74ac8a9235f32eb8bbd75300ca4
dollarstir <kpin463@gmail.com>
2022-07-29 12:53:29 +0000

    sdfdsf

--- main/nasea.php
+++ main/nasea.php
@@ -498,6 +498,20 @@ begin('Welcome');
 
 
     <!-- all js here -->
+
+	<div id='tawk_62e3d52b54f06e12d88bed32'></div>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_62e3d52b54f06e12d88bed32';
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/62e3d52b54f06e12d88bed32/1g9504tgu';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);})();
+</script>
+<!--End of Tawk.to Script-->
     <?php scripts(); ?>
 </body>
 
